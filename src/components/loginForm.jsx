8eef97c1522b1e0f6478928f27d6d0e538dd4f20
JayDub21 <justin.wofford.21@gmail.com>
2021-01-12 00:16:23 -0600

    added error messages for blank input

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -4,19 +4,40 @@ import Input from './common/input';
 class LoginForm extends Component {
   state = {
     account: { username: '', password: '' },
+    errors: {},
+  };
+
+  validate = () => {
+    const errors = {};
+
+    const { account } = this.state;
+    if (account.username.trim() === '')
+      errors.username = 'Username is required';
+    if (account.password.trim() === '')
+      errors.password = 'Password is required';
+
+    return Object.keys(errors).length === 0 ? null : errors;
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
+
+    const errors = this.validate();
+    console.log(errors);
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
     console.log('Submitted');
   };
+
   handleChange = ({ currentTarget: input }) => {
     const account = { ...this.state.account };
     account[input.name] = input.value;
     this.setState({ account });
   };
+
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
         <h2>Login</h2>
@@ -26,12 +47,14 @@ class LoginForm extends Component {
             value={account.username}
             label='Username'
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name='password'
             value={account.password}
             label='Password'
             onChange={this.handleChange}
+            error={errors.password}
           />
 
           <button className='btn btn-primary'>Login</button>
