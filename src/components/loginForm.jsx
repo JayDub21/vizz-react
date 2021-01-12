6095179b7ab72ee57f8f40cd64f35b9b8388b92c
JayDub21 <justin.wofford.21@gmail.com>
2021-01-11 23:56:47 -0600

    made input component & added it login Form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Input from './common/input';
 
 class LoginForm extends Component {
   state = {
@@ -20,28 +21,19 @@ class LoginForm extends Component {
       <div>
         <h2>Login</h2>
         <form onSubmit={this.handleSubmit}>
-          <div className='from-group'>
-            <label htmlFor='username'>Username</label>
-            <input
-              value={account.username}
-              onChange={this.handleChange}
-              name='username'
-              id='username'
-              type='text'
-              className='form-control'
-            />
-          </div>
-          <div className='from-group'>
-            <label htmlFor='password'>Password</label>
-            <input
-              value={account.password}
-              onChange={this.handleChange}
-              id='password'
-              name='password'
-              type='text'
-              className='form-control'
-            />
-          </div>
+          <Input
+            name='username'
+            value={account.username}
+            label='Username'
+            onChange={this.handleChange}
+          />
+          <Input
+            name='password'
+            value={account.password}
+            label='Password'
+            onChange={this.handleChange}
+          />
+
           <button className='btn btn-primary'>Login</button>
         </form>
       </div>
