c906f5865afd9c0ea192dc2b024a125836bd7f64
JayDub21 <justin.wofford.21@gmail.com>
2021-01-11 23:10:54 -0600

    login setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import NavBar from './components/navBar.jsx';
+import LoginForm from './components/loginForm';
 import Movies from './components/movies';
+import MovieForm from './components/movieForm';
 import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import './App.css';
-import { render } from '@testing-library/react';
 
 function App() {
   return (
@@ -14,7 +15,9 @@ function App() {
       <NavBar />
       <main className='container'>
         <Switch>
-          <Route path='/movies/:id?' component={Movies} />
+          <Route path='/login' component={LoginForm} />
+          <Route path='/movies/:id' component={MovieForm} />
+          <Route path='/movies/' component={Movies} />
           <Route path='/customers' component={Customers} />
           <Route path='/rentals' component={Rentals} />
           <Route path='/not-found' component={NotFound} />
---
+++ src/components/loginForm.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+  render() {
+    return <h2>Login</h2>;
+  }
+}
+
+export default LoginForm;
