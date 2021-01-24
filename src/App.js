1bc33e40c9ce749554c234d03b370fe97ffe14f7
JayDub21 <justin.wofford.21@gmail.com>
2021-01-24 12:46:01 -0600

    added logout component, and added route to app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { ToastContainer } from 'react-toastify';
 import jwtDecode from 'jwt-decode';
 import NavBar from './components/navBar.jsx';
 import LoginForm from './components/loginForm';
+import Logout from './components/logout';
 import RegisterForm from './components/registerForm';
 import Movies from './components/movies';
 import MovieForm from './components/movieForm';
@@ -35,6 +36,7 @@ class App extends Component {
           <Switch>
             <Route path='/register' component={RegisterForm} />
             <Route path='/login' component={LoginForm} />
+            <Route path='/logout' component={Logout} />
             <Route path='/movies/:id' component={MovieForm} />
             <Route path='/movies/' component={Movies} />
             <Route path='/customers' component={Customers} />
