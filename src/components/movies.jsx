059849b9e6d6ac0b90e76d969a18888795e3a130
JayDub21 <justin.wofford.21@gmail.com>
2021-01-26 11:36:32 -0600

    created protectedRoute.js & added to new movie, login

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -97,6 +97,7 @@ class Movies extends Component {
   render() {
     const { length: count } = this.state.movies;
     const { pageSize, currentPage, sortColumn, searchQuery } = this.state;
+    const { user } = this.props;
 
     if (count === 0) return <p>There are no movies in the database.</p>;
 
@@ -112,9 +113,11 @@ class Movies extends Component {
           />
         </div>
         <div className='col'>
-          <Link to='/movies/new'>
-            <button className='btn btn-primary mb-2'>New Movie</button>
-          </Link>
+          {user && (
+            <Link to='/movies/new'>
+              <button className='btn btn-primary mb-2'>New Movie</button>
+            </Link>
+          )}
 
           <p>Showing {totalCount} movies in the database.</p>
           <SearchBox value={searchQuery} onChange={this.handleSearch} />
